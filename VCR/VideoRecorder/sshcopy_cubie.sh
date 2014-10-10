0a99ce0a3eeb78b8919966e3aa6d185a12bc5052
matze <matze@localhost.localdomain>
2014-10-10 18:59:14 +0000

    Cleanup

--- VCR/VideoRecorder/sshcopy_cubie.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#copies the tar to the recorder
-
-scp $1 root@cubie:/root/Recorder
--- VCR/VideoRecorder/sshcopy_recoder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#copies the tar to the recorder
-
-scp $1 matze@recorder:/home/matze/Recorder
-
